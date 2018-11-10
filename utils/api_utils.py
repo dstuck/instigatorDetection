6a8bdacd9031cc52e8269f23c694739fbaf9e48e
dstuck <dstuck@evidation.com>
2018-11-10 11:32:50 -0800

    DSt: Add Api subclass to accept serialized requests

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -1,13 +1,23 @@
 from os import environ
 
-import twitter
+from resilient_api.direct_twitter_api import DirectTwitterApi
 from yaml import safe_load
 
 def get_credentials():
+    """
+    Pull credentials from $TWITTER_CREDENTIAL_FILE or /etc/twitter_api/credentials.yml
+    :return: credential dict
+    """
     credential_file = environ.get('TWITTER_CREDENTIAL_FILE', '/etc/twitter_api/credentials.yml')
     with open(credential_file) as f:
         credentials = safe_load(f.read())
     return credentials
 
-def get_api():
-    return twitter.Api(application_only_auth=True, **get_credentials())
+def get_api(credentials=None):
+    """
+    Get api from credentials. Default credentials pulled from $TWITTER_CREDENTIAL_FILE
+    :param credentials: Dict specifying "consumer_key" and "consumer_secret"
+    :return: twitter Api object
+    """
+    credentials = credentials or get_credentials()
+    return DirectTwitterApi(application_only_auth=True, **credentials)
