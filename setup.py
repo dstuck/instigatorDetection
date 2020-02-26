2775c9b7548b8404abcf704b33f445d45fe76404
dstuck <david.e.stuck@gmail.com>
2020-02-26 11:19:33 -0600

    Fix security issue

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         "python-twitter",
         "pyyaml",
-        "SQLAlchemy==1.2.11",
+        "SQLAlchemy>=1.3.0",
         "alembic==1.0.0",
         "psycopg2==2.7.5",
     ],
